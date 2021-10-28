3ac62082ba5a5fe0cf9fe96d30062c232c5e38c7
Petri Järvenpää <petri.jarvenpaa@outlook.com>
2021-10-28 13:45:15 +0300

    register ja login toiminta

--- src/components/Pages/ProductCard.js
+++ src/components/Pages/ProductCard.js
@@ -1,45 +1,21 @@
+import "../../App.css";
 import {Card, Col, Button} from 'react-bootstrap';
 import { CartState } from "../../context/Context";
 import { Link } from "react-router-dom";
 
-
-
-
-
 const ProductCard = ({item}) => { //Shop sivulla lähetettiin state ja täällä käyttöön
-    
     
     const {
         state: { cart },
-        dispatch,
+        dispatch, 
     } = CartState();
-
-
-
     console.log(cart);
-
-
-
-    
-    //Card kuvan koko määritys
-    const imgSize = {
-        width: '120px',
-        margin: '0 auto',
-        padding: '10px 0',
-        height: '150px'
-    }
-    //Card muokkaus, pituus ja varjostus
-    const cardStyle = {
-        height: '97%',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-        padding: "20px 0"
-    }
 
     //Palautetaan Bootsrapin Card jossa on item.id, title, price
     return(
             <Col xs={12} md={6} xl={4}>
-                <Card style={cardStyle} >
-                    <Card.Img variant="top" style={imgSize} src={item.image} />
+                <Card className="cardStyle">
+                    <Card.Img variant="top" className="imgSize"  src={item.image} />
                     <Card.Body>
                         <Card.Title>
                             <h5 style={{fontSize: '1rem'}}>{item.title}</h5>
